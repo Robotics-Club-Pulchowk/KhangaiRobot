5eed70660ad232b528f1636c9372e8f11f03671c
n-is <073bex422.nischal@pcampus.edu.np>
2019-04-28 23:54:34 +0545

    Generate code for logging using usart2. Logging code not yet tested.

--- Core/Src/utils/logger.cpp
+++ Core/Src/utils/logger.cpp
@@ -1,31 +1,40 @@
 
 #include "stm32f4xx_hal.h"
 #include "queue_custom.h"
+#include "usart.h"
 
 extern "C" int ITM_SendString(char *data, int len);
 
-Queue<char, 32*1024> gPrintfData;
+uint8_t gPrintfData[2][16*1024];
+uint32_t gPrintfDataIndex[2] = { 0 };
 
-// volatile uint8_t gPrintfData[32*1024];
-// volatile uint32_t gPrintfDataIndex = 0;
+uint8_t gSending_Index = 0;
+volatile bool gPrintfDataSent = true;
 
 int ITM_SendString(char *data, int len)
 {
         int i = 0;
         for (; i < len; ++i)
         {
-                // gPrintfData[gPrintfDataIndex++] = data[i];
-                ITM_SendChar(data[i]);
-                // gPrintfData.insert(data[i]);
+                uint8_t buf_index = 1 - gSending_Index;
+                gPrintfData[buf_index][gPrintfDataIndex[buf_index]++] = data[i];
         }
         return i;
 }
 
-void send_AllData()
+void log_data()
 {
-        // for (uint32_t i = 0; i < gPrintfDataIndex; ++i) {
-        //         ITM_SendChar(gPrintfData[i]);
-        //         asm volatile("nop");
-        //         asm volatile("nop");
-        // }
+        if (gPrintfDataSent) {
+                uint8_t send_index = 1 - gSending_Index;
+                gSending_Index = 1 - gSending_Index;
+
+                HAL_UART_Transmit_DMA(&huart2, gPrintfData[send_index], gPrintfDataIndex[send_index]);
+                gPrintfDataIndex[send_index] = 0;
+                gPrintfDataSent = false;
+        }
+}
+
+void Logging_Handle_TxCplt()
+{
+        gPrintfDataSent = true;
 }
