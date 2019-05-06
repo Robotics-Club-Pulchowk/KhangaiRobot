3d99fe7bc4ef31f33ac7f5ae001b263ef3bdbf65
n-is <073bex422.nischal@pcampus.edu.np>
2019-05-06 14:53:21 +0545

    Use linear spline for only fields A and O.

--- Core/Src/parts/processor/robo_states.cpp
+++ Core/Src/parts/processor/robo_states.cpp
@@ -132,8 +132,7 @@ Vec2<float> Robo_States::calc_Velocity(Vec3<float> state, Vec3<float> vel_from_b
         Vec2<float> velocity;
 
         Field id = sv_->id;
-        if (id == Field::FIELD_A || id == Field::FIELD_I ||
-            id == Field::FIELD_O || id == Field::FIELD_P) {
+        if (id == Field::FIELD_A || id == Field::FIELD_O) {
                 float v = calc_RoboVelocity(state, dt_millis);
                 float theta = calc_AngleOfAttack(state, v, dt_millis);
                 velocity.set_Values(v, theta);
