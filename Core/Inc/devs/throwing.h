8af0653054761264190401074cac6b9c257007af
n-is <073bex422.nischal@pcampus.edu.np>
2019-05-21 00:21:40 +0545

    Add communication codes with throwing.

---
+++ Core/Inc/devs/throwing.h
@@ -0,0 +1,47 @@
+/*
+ * throwing.h
+ * 
+ * Created : 5/18/2019
+ *  Author : n-is
+ *   email : 073bex422.nischal@pcampus.edu.np
+ */
+
+#ifndef _THROWING_H_
+#define _THROWING_H_
+
+#include "queue_custom.h"
+
+
+enum class Throwing_Commands
+{
+        GRIP = 0x01,
+        ACTUATE = 0x02,
+        EXTEND = 0x03,
+        PASS_GEREGE = 0x04,
+        THROW = 0x05,
+        RETRIEVE = 0x06
+};
+
+class Throwing
+{
+public:
+        Throwing(Throwing &&) = default;
+        Throwing(const Throwing &) = default;
+        Throwing &operator=(Throwing &&) = default;
+        Throwing &operator=(const Throwing &) = default;
+        ~Throwing() { }
+        
+        static Throwing& get_Instance();
+        
+        int init();
+        int write(uint8_t cmd);
+
+private:
+        Throwing() { }
+};
+
+
+void Throwing_Handle_TxCplt(void);
+
+
+#endif //! _THROWING_H_
---
+++ Core/Src/devs/throwing.cpp
@@ -0,0 +1,62 @@
+/*
+ * throwing.cpp
+ * 
+ * Created : 5/18/2019
+ *  Author : n-is
+ *   email : 073bex422.nischal@pcampus.edu.np
+ */
+
+#include "throwing.h"
+#include "usart.h"
+
+#define THROWING_UART   (huart4)
+
+#define MAX_PACKET_LENGTH       (2)
+#define MAX_COMMAND_QUEUE       (2)
+
+uint8_t gThrowing_TxData[MAX_PACKET_LENGTH];
+
+Throwing& Throwing::get_Instance()
+{
+        static Throwing sThrowing_Instance;
+
+        return sThrowing_Instance;
+}
+
+int Throwing::init()
+{
+        __HAL_UART_ENABLE_IT(&THROWING_UART, UART_IT_TC);
+
+        gThrowing_TxData[0] = 0xA5;
+}
+
+Queue<uint8_t, MAX_COMMAND_QUEUE> gThrowing_Commands;
+
+uint8_t gThrowing_TxBuffer[MAX_PACKET_LENGTH];
+bool gAll_Command_Sent = true;
+
+int Throwing::write(uint8_t cmd)
+{
+        if (gAll_Command_Sent) {
+                gAll_Command_Sent = false;
+
+                gThrowing_TxData[1] = cmd;
+                HAL_UART_Transmit_IT(&THROWING_UART, gThrowing_TxData, 2);
+        }
+        else {
+                gThrowing_Commands.insert(cmd);
+        }
+
+        return 0;
+}
+
+void Throwing_Handle_TxCplt(void)
+{
+        if (!gThrowing_Commands.is_Empty()) {
+                gThrowing_TxData[1] = gThrowing_Commands.lookup();
+                HAL_UART_Transmit_IT(&THROWING_UART, gThrowing_TxData, 2);
+        }
+        else {
+                gAll_Command_Sent = true;
+        }
+}
