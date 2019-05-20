610d44f8db0f2df3e71f579e5c33d8745e406d69
n-is <073bex422.nischal@pcampus.edu.np>
2019-05-21 00:20:48 +0545

    Generate codes for supporting communication with throwing.

--- Core/Src/sys/stm32f4xx_it.c
+++ Core/Src/sys/stm32f4xx_it.c
@@ -76,6 +76,7 @@ extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
 extern DMA_HandleTypeDef hdma_usart2_rx;
 extern DMA_HandleTypeDef hdma_usart2_tx;
 extern DMA_HandleTypeDef hdma_usart3_rx;
+extern UART_HandleTypeDef huart4;
 extern UART_HandleTypeDef huart3;
 extern TIM_HandleTypeDef htim5;
 
@@ -277,6 +278,20 @@ void TIM5_IRQHandler(void)
   /* USER CODE BEGIN TIM5_IRQn 1 */
 
   /* USER CODE END TIM5_IRQn 1 */
+}
+
+/**
+  * @brief This function handles UART4 global interrupt.
+  */
+void UART4_IRQHandler(void)
+{
+  /* USER CODE BEGIN UART4_IRQn 0 */
+
+  /* USER CODE END UART4_IRQn 0 */
+  HAL_UART_IRQHandler(&huart4);
+  /* USER CODE BEGIN UART4_IRQn 1 */
+
+  /* USER CODE END UART4_IRQn 1 */
 }
 
 /**
