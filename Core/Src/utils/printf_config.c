d4fd350c727cb8d6dd44cdb2e312719c22547579
n-is <073bex422.nischal@pcampus.edu.np>
2019-05-22 22:14:23 +0545

    Use ITM for printf.

--- Core/Src/utils/printf_config.c
+++ Core/Src/utils/printf_config.c
@@ -8,7 +8,16 @@
 
 #include "stm32f4xx_hal.h"
 
-int ITM_SendString(char *data, int len);
+
+int ITM_SendString(char *data, int len)
+{
+        int i = 0;
+        for (; i < len; ++i)
+        {
+                ITM_SendChar(data[i]);
+        }
+        return i;
+}
 
 int _write(int file, char *data, int len)
 {
