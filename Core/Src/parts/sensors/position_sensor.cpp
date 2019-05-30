393a90570cefcb129b14b845501ce68c7146f1f0
n-is <073bex422.nischal@pcampus.edu.np>
2019-05-30 22:38:46 +0545

    Refactor code. Remove Lidar Biases. They are to be moved to arduino. Separate encoder rotating codes to separate function.

--- Core/Src/parts/sensors/position_sensor.cpp
+++ Core/Src/parts/sensors/position_sensor.cpp
@@ -28,14 +28,12 @@ static float gLast_XEncoderValue = 0;
 
 //*
 
-static float gXLidar_Bias = 0;
-static float gYLidar_Bias = 0;
-
 // Last Position
 Vec3<float> gLastPosition(gLast_XEncoderValue, gLast_YEncoderValue, 0);
 
 Kalman_Vars gEncoders_KV;
 Kalman_Vars gXLidarEncoder_KV;
+Kalman_Vars gYLidarEncoder_KV;
 
 
 PositionSensor& PositionSensor::get_Instance()
@@ -48,10 +46,7 @@ PositionSensor& PositionSensor::get_Instance()
 int PositionSensor::init(uint32_t dt_millis)
 {
         Encoders_Init();
-
-        gXEncoder.init();
-        gYEncoder.init();
-        gXLidar.init();
+        Lidars_Init();
 
         return 0;
 }
@@ -60,7 +55,7 @@ Vec3<float> PositionSensor::read_Position(Vec3<float> ori, Vec3<float> base_stat
 {
         // First Collect Data from all available position sensors
         // 0 -> x, 1 -> y
-        Vec3<float> free_wheel;
+        Vec2<float> free_wheel;
         float lidar[2];
 
         bool x_lidar_used(false), y_lidar_used(false);
@@ -76,15 +71,16 @@ Vec3<float> PositionSensor::read_Position(Vec3<float> ori, Vec3<float> base_stat
                         y_enc_used = true;
                 }
                 else if (p_sensors_[i]->get_Name() == SensorName::XLidar) {
-                        lidar[0] = p_sensors_[i]->read() + gXLidar_Bias;
+                        lidar[0] = p_sensors_[i]->read();
                         x_lidar_used = true;
                         // printf("Time : %ld\tLidar : %ld\n", HAL_GetTick(), (int32_t)lidar[0]);
                 }
                 else if (p_sensors_[i]->get_Name() == SensorName::YLidar) {
-                        lidar[1] = p_sensors_[i]->read() + gYLidar_Bias;
+                        lidar[1] = p_sensors_[i]->read();
                         y_lidar_used = true;
                 }
         }
+        // Report error if there are no available sensors in any axes
         if (!(x_lidar_used || x_enc_used)) {
                 error(Error::DEVICE_ERROR);
                 printf("No Data Available on x-direction!!\n");
@@ -97,6 +93,7 @@ Vec3<float> PositionSensor::read_Position(Vec3<float> ori, Vec3<float> base_stat
         if (x_lidar_used || y_lidar_used) {
                 process_LidarData(lidar, sv);
         }
+
         // Smooth Lidar Data if used else clear the smoothie
         if (x_lidar_used) {
                 lidar[0] = gXLidarAlpha35.smooth(lidar[0]);
@@ -104,43 +101,18 @@ Vec3<float> PositionSensor::read_Position(Vec3<float> ori, Vec3<float> base_stat
         else {
                 gXLidarAlpha35.clear();
         }
-        // if (y_lidar_used) {
-
-        // }
-        // else {
-
-        // }
+        if (y_lidar_used) {
+                lidar[1] = gYLidarAlpha35.smooth(lidar[1]);
+        }
+        else {
+                gYLidarAlpha35.clear();
+        }
 
 
         // Calculate the movement of the body with respect to the body frame
-
-        // Rotate the movement to the navigation frame
-        // yaw-pitch-roll
-        float roll = ori.getX() / 57.3;
-        float pitch = ori.getY() / 57.3;
-        float yaw = ori.getZ() / 57.3;
-
-        float s_r = sin(roll);
-        float c_r = cos(roll);
-        float s_p = sin(pitch);
-        float c_p = cos(pitch);
-        float s_y = sin(yaw);
-        float c_y = cos(yaw);
-
-        // https://ocw.mit.edu/courses/mechanical-engineering/2-017j-design-of-electromechanical-robotic-systems-fall-2009/course-text/MIT2_017JF09_ch09.pdf
-        // R(r, p, y)
-        float R[3][3] = { { c_p*c_r,                c_p*s_r,               -s_p},
-                          {-c_y*s_r + s_y*s_p*c_r,  c_y*c_r + s_y*s_p*s_r,  s_y*c_p},
-                          { s_y*s_r + c_y*s_p*c_r, -s_y*c_r + c_y*s_p*s_r,  c_y*c_p}, };
-
-        Mat Rm(R);
-
-        Mat d_pos = Rm.trans() * free_wheel;
-
-        Vec3<float> del_pos(d_pos.at(0,0), d_pos.at(1,0), d_pos.at(2,0));
-
-        float ex = del_pos.getX();
-        float ey = del_pos.getY();
+        Vec2<float> enc = rotate_EncData(ori, free_wheel);
+        float ex = enc.getX();        
+        float ey = enc.getY();        
 
         // Fuse the data with the data from lidar that gives movement with
         // respect to the navigation frame
@@ -151,16 +123,14 @@ Vec3<float> PositionSensor::read_Position(Vec3<float> ori, Vec3<float> base_stat
                 xlidar_enc_fuser_.clear();
         }
         else {
-                x = xlidar_enc_fuser_.filter(lidar[0], del_pos.getX(), dt_millis);
+                x = xlidar_enc_fuser_.filter(lidar[0], ex, dt_millis);
         }
-
-        // y lidar not used
         if (!y_lidar_used) {
                 y = gLastPosition.getY() + ey;
-                // ylidar_enc_fuser_.clear();
+                ylidar_enc_fuser_.clear();
         }
         else {
-                // y = ylidar_enc_fuser_.filter(lidar[0], del_pos.getY(), dt_millis);
+                y = ylidar_enc_fuser_.filter(lidar[0], del_pos.getY(), dt_millis);
         }
 
         // x and y are in mm
@@ -242,6 +212,44 @@ void PositionSensor::process_LidarData(float (&lidar)[2], const State_Vars *sv)
         }
 }
 
+Vec2<float> PositionSensor::rotate_EncData(Vec3<float> ori, Vec2<float> enc)
+{
+        // Rotate the movement to the navigation frame
+        // yaw-pitch-roll
+
+        float roll = ori.getX() / 57.3;
+        float pitch = ori.getY() / 57.3;
+        float yaw = ori.getZ() / 57.3;
+
+        float s_r = sin(roll);
+        float c_r = cos(roll);
+        float s_p = sin(pitch);
+        float c_p = cos(pitch);
+        float s_y = sin(yaw);
+        float c_y = cos(yaw);
+
+        // Converting to Vec3 since we only have support for multiplication
+        // between nx3 matrix and a vec3
+        Vec3<float> free_wheel(enc.getX(), enc.getY(), 0);
+
+        // https://ocw.mit.edu/courses/mechanical-engineering/2-017j-design-of-electromechanical-robotic-systems-fall-2009/course-text/MIT2_017JF09_ch09.pdf
+        // R(r, p, y)
+        float R[3][3] = { { c_p*c_r,                c_p*s_r,               -s_p},
+                          {-c_y*s_r + s_y*s_p*c_r,  c_y*c_r + s_y*s_p*s_r,  s_y*c_p},
+                          { s_y*s_r + c_y*s_p*c_r, -s_y*c_r + c_y*s_p*s_r,  c_y*c_p}, };
+
+        Mat Rm(R);
+
+        Mat d_pos = Rm.trans() * free_wheel;
+
+        Vec3<float> del_pos(d_pos.at(0,0), d_pos.at(1,0), d_pos.at(2,0));
+
+        float ex = del_pos.getX();
+        float ey = del_pos.getY();
+
+        return Vec2<float>(ex, ey);
+}
+
 
 int init_XLidarEncoderKalman(uint32_t dt_millis)
 {
@@ -273,7 +281,7 @@ int init_XLidarEncoderKalman(uint32_t dt_millis)
         process_error.at(1,1) = 0.003;
         
         Mat measure_error(1,1);
-        measure_error.at(0,0) = 200;
+        measure_error.at(0,0) = 5;
 
         gXLidarEncoder_KV.set_F(state_model);
         gXLidarEncoder_KV.set_B(control_model);
@@ -282,6 +290,50 @@ int init_XLidarEncoderKalman(uint32_t dt_millis)
         gXLidarEncoder_KV.set_P(priori_error);
         gXLidarEncoder_KV.set_Q(process_error);
         gXLidarEncoder_KV.set_R(measure_error);
+
+        return 0;
+}
+
+
+int init_YLidarEncoderKalman(uint32_t dt_millis)
+{
+        // This is for data fusion of lidar and encoder
+        Mat state_model(2,2);
+        state_model.at(0,0) = 1;
+        state_model.at(0,1) = -1;
+        state_model.at(1,0) = 0;
+        state_model.at(1,1) = 1;
+
+        Mat control_model(2,1);
+        control_model.at(0,0) = 1;
+        control_model.at(1,0) = 0;
+        
+        Mat obs_model(1,2);
+        obs_model.at(0,0) = 1;
+        obs_model.at(0,1) = 0;
+
+        Mat priori_error(2,2);
+        priori_error.at(0,0) = 500;
+        priori_error.at(0,1) = 0;
+        priori_error.at(1,0) = 0;
+        priori_error.at(1,1) = 500;
+        
+        Mat process_error(2,2);
+        process_error.at(0,0) = 0.001;
+        process_error.at(0,1) = 0;
+        process_error.at(1,0) = 0;
+        process_error.at(1,1) = 0.003;
+        
+        Mat measure_error(1,1);
+        measure_error.at(0,0) = 5;
+
+        gYLidarEncoder_KV.set_F(state_model);
+        gYLidarEncoder_KV.set_B(control_model);
+        gYLidarEncoder_KV.set_H(obs_model);
+        gYLidarEncoder_KV.set_I(2);
+        gYLidarEncoder_KV.set_P(priori_error);
+        gYLidarEncoder_KV.set_Q(process_error);
+        gYLidarEncoder_KV.set_R(measure_error);
 
         return 0;
 }
