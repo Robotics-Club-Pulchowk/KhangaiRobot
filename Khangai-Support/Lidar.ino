688dd35202b68d24b0a1e9c41d3a6b4585213744
n-is <073bex422.nischal@pcampus.edu.np>
2019-06-14 15:21:41 +0545

    Update Lidar Bias for Blue Field.

--- Khangai-Support/Lidar.ino
+++ Khangai-Support/Lidar.ino
@@ -16,8 +16,8 @@ static unsigned long gYLidar_Read_Time = 0;
 static bool gIs_First_Update = true;
 
 //* Biases of respective lidars
-static unsigned long gXLidar_Bias = 135;
-static unsigned long gYLidar_Bias = 35;
+static unsigned long gXLidar_Bias = 0;
+static unsigned long gYLidar_Bias = 90;
 
 //* Construct the lidar's object
 TFMini gXLidar;
