6f227fd19d8bcf0462e3b7d00620d86d7b70cab5
n-is <073bex422.nischal@pcampus.edu.np>
2019-06-26 22:43:14 +0545

    Add Code to detect unusual behaviour of moving with last input without pid.

--- Core/Src/sys/stm32f4xx_it.c
+++ Core/Src/sys/stm32f4xx_it.c
@@ -111,6 +111,7 @@ void HardFault_Handler(void)
   while (1)
   {
     /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    HAL_GPIO_WritePin(B_RedLED_GPIO_Port, B_RedLED_Pin, GPIO_PIN_SET);
     /* USER CODE END W1_HardFault_IRQn 0 */
   }
 }
